0921603736ed4aea36cc3a2f31e51da7479e82b9
Jasper Wang <yu.h.wang@gmail.com>
2021-08-13 22:49:16 +1000

    change to use context

--- cdk-eks/bin/cdk-eks.ts
+++ cdk-eks/bin/cdk-eks.ts
@@ -9,12 +9,13 @@ const env = {
 };
 
 const app = new cdk.App();
+const clusterName = app.node.tryGetContext('cluster-name');
+const vpcId = app.node.tryGetContext('vpc-id');
+
 new CdkEksStack(app, 'CdkEksStack-AutoK8sControls', {
     env: env,
-    // clusterName: 'your-cluster-name',  // if you don't specify the EKS cluster name, CDK will create the name for you.
-    clusterName: 'cdk-auto-k8s-controls',
-    //vpcId: 'your-vpc-id',
-    vpcId: 'vpc-0cc19710c1260b111',
+    clusterName,
+    vpcId,
     // If you speficy an existing vpc-id, then CDK will use this existing VPC to create EKS cluster instead of 
     // creating a new VPC. Please note when using an existing VPC, please make sure its private subnets have been tagged with 
     // "kubernetes.io/role/internal-elb: 1" as described in https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html
